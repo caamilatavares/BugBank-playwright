a04d44dfdf2697e8589628cccb89fdaacb71bc56
Camila Tavares <camisstc@gmail.com>
2026-04-07 19:57:30 -0300

    Adding screenshot on report

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,9 @@ export default defineConfig({
     baseURL: 'https://bugbank.netlify.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
